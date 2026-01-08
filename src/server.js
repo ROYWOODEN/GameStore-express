e1663c69ee953a52c0d1c36dcffd4c274a190b59
ROYWOODEN <ronald.brooks2006@gmail.com>
2026-01-08 14:09:48 +0300

    refactor/api-validation-errors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// const app = require('./app');
 import { app } from './app.js';
 
 const PORT = 5000;
--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,6 +1,8 @@
 export class AppError extends Error {
-  constructor(message, name = 'AppError') {
+  constructor(message, name = 'AppError', userMessage = null, status = 500) {
     super(message);
     this.name = name;
+    this.status = status;
+    this.userMessage = userMessage;
   }
 }
