b056a777c45d7bda4d68d7cf10fd0d6652821aa2
ROYWOODEN <ronald.brooks2006@gmail.com>
2026-04-28 16:00:59 +0300

    feat(user): add avatar management endpoints and harden upload handling

--- src/modules/files/index.js
+++ src/modules/files/index.js
@@ -1,4 +1,5 @@
 import { FILE_TARGETS } from './config/files.config.js';
+import { buildTargetFileUrl } from './utils/file-url.js';
 import { cleanupUploadedFiles, cleanupTargetUrls } from './services/file-cleanup.service.js';
 
-export { FILE_TARGETS, cleanupUploadedFiles, cleanupTargetUrls };
+export { FILE_TARGETS, buildTargetFileUrl, cleanupUploadedFiles, cleanupTargetUrls };
--- src/modules/user/routes/user.routes.js
+++ src/modules/user/routes/user.routes.js
@@ -1,15 +1,25 @@
 import { Router } from 'express';
 import { authorize } from '#src/modules/Auth/helpers/authorize.helper.js';
+import { withUpload } from '#src/middleware/upload/with-upload.middleware.js';
 import {
   handleDeleteCurrentUser,
+  handleDeleteCurrentUserAvatar,
   handleGetCurrentUser,
   handleUpdateCurrentUser,
+  handleUpdateCurrentUserAvatar,
 } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
 userRouter.get('/users/me', ...authorize(), handleGetCurrentUser);
 userRouter.patch('/users/me', ...authorize(), handleUpdateCurrentUser);
+userRouter.patch(
+  '/users/me/avatar',
+  ...authorize(),
+  withUpload({ type: 'user_avatars', field: 'avatar', maxCount: 1 }),
+  handleUpdateCurrentUserAvatar,
+);
+userRouter.delete('/users/me/avatar', ...authorize(), handleDeleteCurrentUserAvatar);
 userRouter.delete('/users/me', ...authorize(), handleDeleteCurrentUser);
 
 export { userRouter };
